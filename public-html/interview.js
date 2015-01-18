d5ca915d0470e1d7dcc004317965fa37cdd89e7c
azguard98 <bdp.edu@gmail.com>
2015-01-17 21:26:06 -0500

    added video prompt/response template

---
+++ public-html/interview.js
@@ -0,0 +1,35 @@
+//Global Variables
+var language = "UNASSIGNED";
+var recording = 0;
+
+//Initialization
+// document.getElementById("content-prompt").style.display = "none";
+
+//after user first logs in, show next prompt
+function beginInterview(userLang) {
+	language = userLang;
+	// alert(language);
+	// document.getElementById("content-wrapper").innerHTML = '<p>You\'ve chosen: ' + language + '.</p>';
+	// document.getElementById("content-wrapper").innerHTML = '<div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" src="http://www.youtube.com/embed/eGilvHv0_bE?rel=0&autoplay=1"></iframe></div>';
+
+	document.getElementById("content-welcome").style.display = "none";
+	document.getElementById("content-prompt-english").style.display = "block";
+}
+
+//when user presses record button
+function toggleAudio() {
+	if (recording == 0) { //waiting
+		recording = 1; //capturing
+		document.getElementById("recording-button").className = "btn btn-lg btn-danger col-md-offset-3";
+	} else {
+		recording = 2; //sending
+		document.getElementById("recording-button").innerHTML = '<span class="glyphicon glyphicon-ok"></span>';
+		document.getElementById("recording-button").className = "btn btn-lg btn-success col-md-offset-3";
+		document.getElementById("next-button").className = "btn btn-lg btn-primary col-md-offset-3";
+	}
+}
+
+//when a new video prompt needs to be inserted and played
+function nextVideo() {
+	document.getElementById("video-frame").src = 'http://www.youtube.com/embed/eGilvHv0_bE?rel=0' + '&autoplay=1';
+}
