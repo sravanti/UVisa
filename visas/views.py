156956b43401b4a05434f916dd10cf52f7daa18f
Sravanti Tekumalla <stekumal@wellesley.edu>
2015-01-20 13:28:08 -0500

    Changed submission for AJAX request instead

--- visas/views.py
+++ visas/views.py
@@ -6,6 +6,8 @@ from visas.forms import *
 from visas.models import *
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 """
 Register / login / logout
@@ -76,14 +78,21 @@ def load_form(request):
 def get_next_question(request, num):
     return Question.objects.get(id=num+1)
 
+@require_http_methods(['POST]')
+@csrf_exempt
 def process_answer(request):
-    if request.method == 'POST':
-        user = request.user
-        form = Form.objects.get(user=user)
-        form_data = FormForm(request.POST)
-        answer_text = form_data.cleaned_data['answer_text']
-        question_num = form_data.question_num
-        form.answers[question_num-1].answer_text = answer_text
-        print answers[question_num-1].answer_text
+    form = Form.objects.get(user=request.user)
+    #change the below to correspond with AJAX request   
+    """
+    form_data = FormForm(request.POST)
+    answer_text = form_data.cleaned_data['answer_text']
+    question_num = form_data.question_num
+    form.answers[question_num-1].answer_text = answer_text
+    print answers[question_num-1].answer_text
+    """
+    form.save()
+    if request.is_ajax():
+        return HttpResponse("")
+    return reverse('visas.views.load_form')
     
  
